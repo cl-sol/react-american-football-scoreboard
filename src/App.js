a73ba8ec62e5f870346c61e32ac5657763c933b8
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-04-22 20:14:10 -0400

    completed a single stretch goal (lel)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   let [lions, setLions] = useState(0);
   let [tigers, setTigers] = useState(0);
-  console.log(App);
+  
+  function handler(teamName, amt) {
+    if (teamName === "lions"){
+      setLions(lions + amt);
+    } else if (teamName === "tigers"){
+      setTigers(tigers + amt);
+    }
+  }
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -30,24 +38,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick = {() => {
-            setLions(lions + 7);
-          }}
-          >Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick = {() => {
-            setLions(lions + 3);
-          }}
-          >Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick = {() => handler("lions", 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick = {() => handler("lions", 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick = {() => {
-            setTigers(tigers + 7);
-          }}
-          >Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick = {() => {
-            setTigers(tigers + 3);
-          }}
-          >Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick = {() => handler("tigers", 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick = {() => handler("tigers", 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
